d1fee36e80a2cc401282eae2b7b275a86494742c
Mamuni98 <nabanitasahoo468@gmail.com>
2023-05-19 01:06:04 +0530

    HomePage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -32,6 +32,59 @@ const Home = () => {
           </div>
         </Container>
       </Navbar>
+      <Container>
+      <div className={classes.items}>
+        <h2>TOUR</h2>
+        <div>
+          <span>JUL16</span>
+          <span>DETROIT, MI</span>
+          <span>DTE ENERGY MUSIC THEATRE</span>
+          <button type="button" className="btn btn-info btn-sm">
+            BUY TICKETS
+          </button>
+        </div>
+        <div>
+          <span>JUL19</span>
+          <span>TORONTO,ON</span>
+          <span>BUDWEISER STAGE</span>
+          <button type="button" className="btn btn-info btn-sm">
+            BUY TICKETS
+          </button>
+        </div>
+        <div>
+          <span>JUL22</span>
+          <span>BRISTOW, VA</span>
+          <span>JIGGY LUBE LIVE</span>
+          <button type="button" className="btn btn-info btn-sm">
+            BUY TICKETS
+          </button>
+        </div>
+        <div>
+          <span>JUL29</span>
+          <span>PHOENIX, AZ</span>
+          <span>AK-CHIN PAVILION</span>
+          <button type="button" className="btn btn-info btn-sm">
+            BUY TICKETS
+          </button>
+        </div>
+        <div>
+          <span>JUL31</span>
+          <span>LAS VEGAS,TC</span>
+          <span>T-MOBILE ARENA</span>
+          <button type="button" className="btn btn-info btn-sm">
+            BUY TICKETS
+          </button>
+        </div>
+        <div>
+          <span>AUG10</span>
+          <span>CONCORD, CA</span>
+          <span>CONCORD PAVILION</span>
+          <button type="button" className="btn btn-info btn-sm">
+            BUY TICKETS
+          </button>
+        </div>
+      </div>
+      </Container>
       <FooterNav />
     </>
   );
